478b7d6b3bf22613da53da4b9ea1c0975987c1fb
mtsue <bolerock309@gmail.com>
2017-11-21 15:56:25 +0900

    Change webpack.config.js to compile js files
    .js and .jsx files are compiled to browser supported js version with Babel.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 const path = require('path');
 
-module.exports = [
-    {
-        context: path.join(__dirname, 'src'),
-        entry: {
-            bundle: './javascripts/index.js'
-        },
-        output: {
-            path: path.join(__dirname, 'dist', 'javascripts'),
-            filename: '[name].js'
-        }
+module.exports = [{
+    context: path.join(__dirname, 'src'),
+    entry: {
+        bundle: './javascripts/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist', 'javascripts'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['env', {
+                            targets: {
+                                browser: 'last 2 versions'
+                            }
+                        }],
+                        'react'
+                    ]
+                }
+            }
+        }]
     }
-];
+}];
